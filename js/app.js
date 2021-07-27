2a9ac63bae2c52fb8746c447ab94d4438c6ce5ff
Hue Minh <nghminh2002@gmail.com>
2021-07-28 00:01:21 +0700

    final version

--- js/app.js
+++ js/app.js
@@ -1,114 +1,119 @@
 /**
- *
+ * 
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- *
+ * 
  * Dependencies: None
- *
+ * 
  * JS Version: ES2015/ES6
- *
+ * 
  * JS Standard: ESlint
- *
- */
+ * 
+*/
 
 /**
  * Define Global Variables
- *
- */
-
+ * 
+*/
+const fragment = document.createDocumentFragment();
+const sectionList = document.querySelectorAll('section');
 
 /**
  * End Global Variables
  * Start Helper Functions
- *
- */
-
-
+ * 
+*/
+function createNavItemHTML(id, name){
+    const itemHTML = `<a class ="menu__link" data-id="${id}">${name}</a>`;
+    return itemHTML;
+}
+function isInViewport (elem) {
+    const bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
 
 /**
  * End Helper Functions
  * Begin Main Functions
- *
- */
+ * 
+*/
 
 // build the nav
-
+function buildNavigation(){
+    for (let i=0; i < sectionList.length; i++){
+        const newMenuItem = document.createElement('li');
+        const sectionName = sectionList[i].getAttribute('data-nav')
+        const sectionId = sectionList[i].getAttribute('id')
+        newMenuItem.innerHTML = createNavItemHTML(sectionId, sectionName)
+         fragment.appendChild(newMenuItem);
+    }
+    const navBarList = document.getElementById('navbar__list')
+    navBarList.appendChild(fragment);
+}
 
 // Add class 'active' to section when near top of viewport
-
+function setActiveClass(){
+    for (let i=0; i < sectionList.length; i++){
+        if (isInViewport(sectionList[i])){
+            sectionList[i].classList.add("your-active-class");
+        }else{
+            sectionList[i].classList.remove("your-active-class");
+        }
+    }
+}
 
 // Scroll to anchor ID using scrollTO event
+function scrollToElement(event){
+    if(event.target.nodeName === 'A'){
+        const sectionId = event.target.getAttribute('data-id');
+        const section = document.getElementById(sectionId);
+        section.scrollIntoView({behavior: "smooth"});
+    }
+}
+
 
 
 /**
  * End Main Functions
  * Begin Events
- *
- */
-
-// Build menu
-
+ * 
+*/
+document.addEventListener('scroll', function(){
+    setActiveClass();
+});
+const navBarList = document.getElementById('navbar__list')
+navBarList.addEventListener('click', function(event){
+    scrollToElement(event)
+})
+// Build menu 
+buildNavigation()
 // Scroll to section on link click
 
 // Set sections as active
 
-// Create text for navigation bar
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
-var node1 = document.createElement("LI");
-var node2 = document.createElement("LI");
-var node3 = document.createElement("LI");
 
-node1.setAttribute("class","section_1");
-node2.setAttribute("class","section_2")
-node3.setAttribute("class","section_3")
-node1.setAttribute("class","menu__link");
-node2.setAttribute("class","menu__link")
-node3.setAttribute("class","menu__link")
+window.onscroll = function() {scrollFunction()};
 
-var a1 = document.createElement("A");
-var a2 = document.createElement("A");
-var a3 = document.createElement("A");
+function scrollFunction() {
+  const divHeight = document.querySelector('#section1').offsetHeight;
+  if (document.body.scrollTop > divHeight || document.documentElement.scrollTop > divHeight) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-a1.href = "#section1";
-a2.href = "#section2";
-a3.href = "#section3";
-
-var section1 = document.createTextNode("SECTION 1");
-var section2 = document.createTextNode("SECTION 2");
-var section3 = document.createTextNode("SECTION 3");
-
-a1.appendChild(section1);
-a2.appendChild(section2);
-a3.appendChild(section3);
-
-node1.appendChild(a1);
-node2.appendChild(a2);
-node3.appendChild(a3);
-
-document.getElementById("navbar__list").appendChild(node1);
-document.getElementById("navbar__list").appendChild(node2);
-document.getElementById("navbar__list").appendChild(node3);
-
-const sections = document.querySelectorAll("section");
-const navLi = document.querySelectorAll('nav .navbar__menu ul li');
-
-window.addEventListener("scroll", ()=> {
-    let current = "";
-
-    sections.forEach( section => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        if (pageYOffset > (sectionTop-sectionHeight/3)){
-            current = section.getAttribute('id');
-        }
-    })
-
-    navLi.forEach( li => {
-        li.classList.remove('active');
-        if (li.classList.contains(current)) {
-            li.classList.add('active')
-        }
-    })
-})
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
