8ea549e4035c71d409b37d3f738f4dba46a9a124
DaiNghia0212 <dainghia12311@gmail.com>
2021-09-05 19:50:52 +0700

    JavaScript-DOM-project

--- js/app.js
+++ js/app.js
@@ -17,16 +17,37 @@
  * Define Global Variables
  *
  */
-
-
+let button = document.getElementById('btn');
+let sections = document.getElementsByTagName('section');
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
-
-
-
+// This function add the navigation bar
+ function addNavBar() {
+    let navbarList = document.getElementById('navbar__list');
+    let list;
+    let anchor;
+    for (let section=1; section<=sections.length; section++) {
+        anchor = document.createElement('a');
+        list = document.createElement('li');
+        anchor.href=`#${sections[section-1].getAttribute('id')}`;
+        anchor.className='menu__link';
+        anchor.innerHTML = `${sections[section-1].getAttribute('data-nav')}`;
+        list.appendChild(anchor);
+        navbarList.appendChild(list);
+    }
+ }
+// This is function make the button appear when we scroll the window to y greater than 200
+function buttonDisplay(){
+    let y=document.documentElement.scrollTop;
+    if (y>800) {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+}
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -34,13 +55,14 @@
  */
 
 // build the nav
-
-
+addNavBar();
+button.onclick = () => window.scrollTo({top: 0, left: 0, behavior: "smooth"});
+//sections[0].onmouseover = () => button.style.display = 'none';
+window.onscroll = buttonDisplay;
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
