b6b939b2b4568acdb4e367d4cf296696e44e2026
nganlam0403 <nganlamforwork@gmail.com>
2021-09-05 19:28:59 +0700

    first commit landing-page-dom

--- js/app.js
+++ js/app.js
@@ -1,55 +1,53 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
+const navItem=document.querySelectorAll('.navbar--item');
+const contentItem=document.querySelectorAll('.content-item');
 
-/**
- * Define Global Variables
- *
- */
+//Add class - remove class
 
+function moveClass(item, name){
+    const tmp=document.querySelector(`.${name}`);
+    if (tmp!=null)tmp.classList.toggle(`${name}`);
+    if (item!=null)item.classList.toggle(`${name}`);
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
+}
 
+//Add event click for each nav item
 
+for (let item of navItem){
+    item.addEventListener('click',function(){
+        moveClass(item,'current');
+    });
+}
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
+//Add even scroll for the body
+document.addEventListener("scroll", function (){
+    var posA = document.body.scrollTop+150;
+    let ok=0;
+    for(let i of contentItem){
+        var posB = i.offsetTop;
+        if (posB < posA){
+            const idName=i.getAttribute('id');
+            const tmp=document.querySelector("[href='#"+idName+"']");
+            const tmp2=document.querySelector("#"+idName);
+            moveClass(tmp,'current');
+            moveClass(tmp2,'your-active-class');
+            ok=1;
+        }
+    }
+    if (!ok){
+        const tmp=document.querySelector('.current');
+        if (tmp!=null)tmp.classList.toggle('current');
+    }
+});
 
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
+// Scroll To Top Button 
+const topBtn=document.querySelector('.scroll-to-top');
+console.log(topBtn);
+const section2=document.querySelector('#section2')
+window.addEventListener('scroll',()=>{
+    if (document.body.scrollTop > (section2.offsetTop-400)){
+        toTop.classList.add('active');
+    }
+    else{
+        toTop.classList.remove('active');
+    }
+})
