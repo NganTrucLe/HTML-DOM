3e4b73754bc2b531457108cf8c0f98393fd09fab
vinhhien119 <vinhhien2k3@gmail.com>
2021-09-05 20:20:45 +0700

    Hien-landingPage

--- js/app.js
+++ js/app.js
@@ -17,39 +17,70 @@
  * Define Global Variables
  *
  */
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
+ const sections = document.querySelectorAll('section');
+ const navLi = document.querySelectorAll('nav li');
+ const button = document.querySelector('.float');
+ 
+ /**
+  * End Global Variables
+  * Start Helper Functions
+  *
+  */
+ 
+ 
+ 
+ /**
+  * End Helper Functions
+  * Begin Main Functions
+  *
+  */
+ window.addEventListener('scroll', () =>{
+     let current = '';
+     let upTop = false;
+     sections.forEach( section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight= section.clientHeight;
+        if (pageYOffset >= (sectionTop-sectionHeight/3)) {
+            current =section.getAttribute('id');
+        }
+     })
+     if (current === 'section2' || current === 'section3') {
+         upTop = true
+         button.classList.add('active');
+     } else {
+         upTop = false
+         button.classList.remove('active')
+     }
+     console.log(upTop);
+     if (upTop === true) {
+         button.classList.add('active');
+     }
+     navLi.forEach(li =>{
+         li.classList.remove('active')
+         if(li.classList.contains(current)) {
+             li.classList.add('active');   
+         }
+     })
+ })
+ 
+ // build the nav
+ 
+ 
+ // Add class 'active' to section when near top of viewport
+ 
+ 
+ // Scroll to anchor ID using scrollTO event
+ 
+ 
+ /**
+  * End Main Functions
+  * Begin Events
+  *
+  */
+ 
+ // Build menu
+ 
+ // Scroll to section on link click
+ 
+ 
+ // Set sections as active
