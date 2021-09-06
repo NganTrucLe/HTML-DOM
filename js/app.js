a5863b145a4e3637d4e572c7a2d8878fb74f15c8
test <huynhnhatkhoa21@gmail.com>
2021-09-06 07:01:43 +0700

    My feature is ready

--- js/app.js
+++ js/app.js
@@ -10,21 +10,25 @@
  * JS Version: ES2015/ES6
  *
  * JS Standard: ESlint
- *
+ 
  */
 
 /**
  * Define Global Variables
- *
+ * 
  */
+ const header=document.querySelector(".page__header");
+ console.log(header.offsetTop);
+ const section=document.getElementsByClassName("landing__container");
+ const sections=document.getElementsByTagName("section");
+ const navbarList=document.getElementById("navbar__list");
+ const list=document.getElementById("navbar__list").children;
 
-
-/**
- * End Global Variables
+/**i
+ * End Global Variabless
  * Start Helper Functions
  *
  */
-
 
 
 /**
@@ -34,13 +38,56 @@
  */
 
 // build the nav
+function buildMenu(){
+    for (let container of section){
+       const items=document.createElement("li");
+       items.textContent=container.firstElementChild.textContent;
+       console.log(items);
+       navbarList.appendChild(items);
+       items.classList.add("menu__link");
+    }
+}
 
 
 // Add class 'active' to section when near top of viewport
-
+function active(){
+   let measure=document.documentElement.scrollTop;
+   //console.log(measure);
+ //  console.log(sections[0].offsetTop);
+   if (measure>0 && measure < sections[0].offsetTop){ 
+    const selectorPainted=document.querySelector("#paint");
+   // console.log(selectorPainted.innerHTML);
+    selectorPainted.removeAttribute("id");
+    let first=navbarList.children[0];
+    first.id="paint";
+   }else if (measure>sections[0].offsetTop && measure < sections[1].offsetTop){
+    const selectorPainted=document.querySelector("#paint");
+  //  console.log(selectorPainted.innerHTML);
+    selectorPainted.removeAttribute("id");
+    let first=navbarList.children[1];
+    first.id="paint";
+   }else if (measure>sections[1].offsetTop && measure < sections[2].offsetTop){
+    const selectorPainted=document.querySelector("#paint");
+ //   console.log(selectorPainted.innerHTML);
+    selectorPainted.removeAttribute("id");
+    let first=navbarList.children[2];
+    first.id="paint";
+   }
+}
 
 // Scroll to anchor ID using scrollTO event
-
+function scroll(e){
+    const onClick=e.target.textContent;
+    for (let idSection of sections){
+      const id=idSection.getAttribute("data-nav");
+      if (onClick==id){
+        window.scrollTo({
+            top: idSection.offsetTop-100,
+            behavior: 'smooth'
+        });
+      }
+    }
+}
 
 /**
  * End Main Functions
@@ -49,7 +96,25 @@
  */
 
 // Build menu
+buildMenu();
 
 // Scroll to section on link click
+navbarList.addEventListener("click", scroll);
+/*navbarList.addEventListener("mouseover",(e)=>{
+  const selector=e.target.textContent;
+  console.log(selector);
+  for (let index of list){
+    if (selector==index.textContent){
+      const selectorPainted=document.querySelector("#paint");
+  //  console.log(selectorPainted.innerHTML);
+      selectorPainted.removeAttribute("id");
+      index.id="paint";
+    }
+  }
+  e.preventDefault();
+})*/
 
+let first=navbarList.children[0];
+first.id="paint";
 // Set sections as active
+window.onscroll = function() {active()};
